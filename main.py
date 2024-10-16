f9d0ef44a489a58575ba352fd2a9fad6fc1318c1
Brandon Young <young.brandon@ufl.edu>
2024-10-16 16:19:00 -0400

    commit and push using pycharm

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#GitHub addition with git bash
+#Simple program that encodes and decodes strings of digits of length 8
 
 #Function developed by Brandon Young
-def password_encoder(password): #password encoder that takes in 8-digit string and returns with each value shifted up
+def password_encoder(password): #Password encoder that takes in 8-digit string and returns with each value shifted up 3
     encoded_password = ''
     for element in str(password):
         element = int(element) + 3
@@ -9,28 +9,44 @@ def password_encoder(password): #password encoder that takes in 8-digit string a
             element = str(element)
             element = element[1]
         encoded_password += str(element)
-    return str(encoded_password)
+    return encoded_password
 
+#Function developed by Brandon Young
+def password_decoder(encoded_password): #Password decoder that takes in 8-digit string and returns with each value shifted down 3
+    decoded_password = ''
+    for element in str(encoded_password):
+        element = int(element) - 3
+        decoded_password += str(element)
+    return decoded_password
 
+#Function developed by Brandon Young
 def main():
+    user_password = '00000000'
+    encoded_password = '00000000'
     while True:
 
         print('Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n')
         user_input = int(input('Please enter an option: '))
 
-        if user_input == 1: #1. User picked encode password
+        if user_input == 1: #1.User picked encode password
+
             user_password = str(input('Please enter your password to encode: '))
-            password_encoder(user_password)
-            print('Your password has been encoded and stored!\n')
+            if len(user_password) == 8:
+                encoded_password = password_encoder(user_password)
+                print('Your password has been encoded and stored!\n')
+            else:
+                print('Password must be of length 8')
 
-        if user_input == 2: #2. User picked decode password
-            pass
+        if user_input == 2: #2.User picked decode password
 
-        if user_input == 3: #3. User picked quit program
+            password_decoder(encoded_password)
+            print(f'The encoded password is {encoded_password}, and the original password is {user_password}\n')
+
+        if user_input == 3: #3.User picked quit program
+
             break
 
 
 
 if __name__ == "__main__":
-
     main()
