66e4babdfda27426487f72a4db63865aa3fb6a3b
MatthewTaner <matthewtaner@ufl.edu>
2024-10-21 15:14:24 -0400

    Added comment above password_decoder method.

--- YounBran_Lab6_Encoder.py
+++ YounBran_Lab6_Encoder.py
@@ -11,7 +11,7 @@ def password_encoder(password): #Password encoder that takes in 8-digit string a
         encoded_password += str(element)
     return encoded_password
 
-
+# function developed by Matthew Taner
 def password_decoder(encoded_password): #Password decoder that takes in 8-digit string and returns with each value shifted down 3
     decoded_password = ''
 
